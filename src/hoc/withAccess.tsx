119498599ae075fca8b6f5358e84fe63e5953eac
FyFka <MoonHeiz@yandex.by>
2022-12-07 14:34:29 +0300

    refactor: incorrect hoc typing

--- src/hoc/withAccess.tsx
+++ src/hoc/withAccess.tsx
@@ -1,14 +1,14 @@
 import { Navigate } from "react-router-dom";
 import { useAppSelector } from "../hooks/useAppSelector";
 
-export default function withAccess<T>(Component: React.FC<T>, isAutorized: boolean, to = "/login") {
-  return (props: Exclude<T, "user">) => {
+export default function withAccess<T>(Component: React.ComponentType<T>, isAutorized: boolean, to = "/login") {
+  return (props: Omit<T, "user">) => {
     const user = useAppSelector((state) => state.user.value);
 
     if ((isAutorized && !user) || (!isAutorized && user)) {
       return <Navigate to={to} />;
     } else {
-      return <Component {...props} user={user} />;
+      return <Component {...(props as T)} user={user} />;
     }
   };
 }
